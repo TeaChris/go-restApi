f9eaf751b3a17d7faab885875d2dd9daad21a18e
bermuda <olasunkanmiboluwatife110@gmail.com>
2024-11-13 08:53:55 +0100

    feat: added gin package

--- main.go
+++ main.go
@@ -3,16 +3,16 @@
  * Path: c:\Users\LENOVO\projects\GO
  * Created Date: Wednesday, November 13th 2024, 8:35:52 am
  * Author: Boluwatife Olasunkanmi O.
- * 
+ *
  * Copyright (c) 2024 PendulumHq
  */
 
 package main
 
-// import (
-//     "github.com/gin-gonic/gin"
-// )
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func main(){
-	
+	gin.Default()
 }
