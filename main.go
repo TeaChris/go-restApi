a1191591919115609f9e2425ae6b1126887a72f9
bermuda <olasunkanmiboluwatife110@gmail.com>
2024-11-13 08:45:49 +0100

    feat: added program entry point

---
+++ main.go
@@ -0,0 +1,18 @@
+/*
+ * Filename: c:\Users\LENOVO\projects\GO\main.go
+ * Path: c:\Users\LENOVO\projects\GO
+ * Created Date: Wednesday, November 13th 2024, 8:35:52 am
+ * Author: Boluwatife Olasunkanmi O.
+ * 
+ * Copyright (c) 2024 PendulumHq
+ */
+
+package main
+
+// import (
+//     "github.com/gin-gonic/gin"
+// )
+
+func main(){
+	
+}
