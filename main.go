a8f48af9ffe3251b31939c43e454a72d4bcbf983
bermuda <olasunkanmiboluwatife110@gmail.com>
2024-11-15 04:55:58 +0100

    feat: first server

--- main.go
+++ main.go
@@ -10,9 +10,21 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main(){
-	gin.Default()
+	server := gin.Default()
+
+	server.GET("/alive", getEvents)
+
+	server.Run(":8080")
+}
+
+func getEvents(context *gin.Context){
+	context.JSON(http.StatusOK, gin.H{
+		"message": "Server is ALIVE!!!",
+	})
 }
