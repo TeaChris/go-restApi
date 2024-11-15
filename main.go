f93a7477123a92f128d32dfb7f5f3bea5ee38b72
bermuda <olasunkanmiboluwatife110@gmail.com>
2024-11-15 05:52:35 +0100

    feat: create and fetch events connection

--- main.go
+++ main.go
@@ -1,16 +1,8 @@
-/*
- * Filename: c:\Users\LENOVO\projects\GO\main.go
- * Path: c:\Users\LENOVO\projects\GO
- * Created Date: Wednesday, November 13th 2024, 8:35:52 am
- * Author: Boluwatife Olasunkanmi O.
- *
- * Copyright (c) 2024 PendulumHq
- */
-
 package main
 
 import (
 	"net/http"
+	"rest-api/models"
 
 	"github.com/gin-gonic/gin"
 )
@@ -18,13 +10,30 @@ import (
 func main(){
 	server := gin.Default()
 
-	server.GET("/alive", getEvents)
+	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
 
 	server.Run(":8080")
 }
 
 func getEvents(context *gin.Context){
-	context.JSON(http.StatusOK, gin.H{
-		"message": "Server is ALIVE!!!",
-	})
+	events := models.GetAllEvents()
+	context.JSON(http.StatusOK, events)
+}
+
+func createEvent(context *gin.Context){
+	var newEvent models.Event
+	err := context.ShouldBindJSON(&newEvent)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	newEvent.ID = 1 // for now
+	newEvent.UserId = 1 // for now
+
+	newEvent.Save()
+	
+	context.JSON(http.StatusCreated, gin.H{"message": "event created successfully", "event": newEvent}) 
 }
