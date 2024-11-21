1fa689b3acb7619877f112c8f668455590bc8bbc
bermuda <olasunkanmiboluwatife110@gmail.com>
2024-11-21 07:34:44 +0100

    feat: updating events

--- routes/events.go
+++ routes/events.go
@@ -56,3 +56,31 @@ func createEvent(context *gin.Context) {
 
 	context.JSON(http.StatusCreated, gin.H{"message": "event created successfully", "event": newEvent})
 }
+
+func updateEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("eventId"), 10, 64)
+
+	if err != nil { // Check if the event ID is valid
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Invalid event ID"})
+		return
+	}
+
+	_, err = models.GetEventById(eventId)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	// Update the event fields
+	var updatedEvent models.Event
+	err = context.ShouldBindJSON(&updatedEvent)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	updatedEvent.ID = eventId
+	updatedEvent.UserId = 1
+}
