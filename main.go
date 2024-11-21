f2cad81b09c22467830383aaafec7d98b132ba0e
bermuda <olasunkanmiboluwatife110@gmail.com>
2024-11-21 07:12:27 +0100

    feat: getting single event

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"rest-api/db"
 	"rest-api/models"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -13,12 +14,13 @@ func main(){
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
+	server.GET("/events/:eventId", getEvent)
 	server.POST("/events", createEvent)
 
 	server.Run(":8080")
 }
 
-func getEvents(context *gin.Context){
+func getEvents(context *gin.Context) {
 	events, err := models.GetAllEvents()
 if err != nil {
 	context.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
@@ -27,7 +29,25 @@ if err != nil {
 	context.JSON(http.StatusOK, events)
 }
 
-func createEvent(context *gin.Context){
+func getEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("eventId"), 10, 64) 
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Invalid event ID"})
+		return
+	}
+
+	event, err := models.GetEventById(eventId)	
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	context.JSON(http.StatusOK, event)
+}
+
+func createEvent(context *gin.Context) {
 	var newEvent models.Event
 	err := context.ShouldBindJSON(&newEvent)
 
