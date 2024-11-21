61097155c4b554c82928778b2c31983ba54bfa82
bermuda <olasunkanmiboluwatife110@gmail.com>
2024-11-21 07:22:36 +0100

    chore: organising code and folders

---
+++ routes/events.go
@@ -0,0 +1,58 @@
+package routes
+
+import (
+	"net/http"
+	"rest-api/models"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+func getEvents(context *gin.Context) {
+	events, err := models.GetAllEvents()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	context.JSON(http.StatusOK, events)
+}
+
+func getEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("eventId"), 10, 64)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Invalid event ID"})
+		return
+	}
+
+	event, err := models.GetEventById(eventId)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	context.JSON(http.StatusOK, event)
+}
+
+func createEvent(context *gin.Context) {
+	var newEvent models.Event
+	err := context.ShouldBindJSON(&newEvent)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	newEvent.ID = 1
+	newEvent.UserId = 1
+
+	err = newEvent.Save()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	context.JSON(http.StatusCreated, gin.H{"message": "event created successfully", "event": newEvent})
+}
