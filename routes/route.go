c8fa6f57f690d559be05d9e364948b5ff98985ef
bermuda <olasunkanmiboluwatife110@gmail.com>
2024-11-29 07:09:27 +0100

    organizing code

--- routes/route.go
+++ routes/route.go
@@ -3,11 +3,11 @@ package routes
 import "github.com/gin-gonic/gin"
 
 func RegisterRoutes(server *gin.Engine) {
-	server.GET("/events", getEvents)
-	server.GET("/events/:eventId", getEvent)
-	server.POST("/events", createEvent)
-	server.PUT("/events/:eventId", updateEvent)
-	server.DELETE("/events/:eventId", deleteEvent)
 	server.POST("/signup", signup)
 	server.POST(("/login"), login)
+	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
+	server.GET("/events/:eventId", getEvent)
+	server.PUT("/events/:eventId", updateEvent)
+	server.DELETE("/events/:eventId", deleteEvent)
 }
