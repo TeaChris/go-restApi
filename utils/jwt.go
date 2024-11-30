9dc4c6b3738c94419401d9070161fddf62291914
bermuda <olasunkanmiboluwatife110@gmail.com>
2024-11-30 07:33:07 +0100

    verify token

--- utils/jwt.go
+++ utils/jwt.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	"errors"
+	"fmt"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -16,4 +18,28 @@ func GenerateToken(email string, userId int64) (string, error) {
 	})
 
 	return token.SignedString([]byte (secret))
+}
+
+
+func VerifyToken(token string) (*jwt.Token, error) {
+	parsedToken, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+
+		if !ok {
+			return nil, errors.New("unexpected signing method")
+		}
+
+		return []byte(secret), nil
+	})
+
+	if err != nil {
+		fmt.Println(err)
+		return nil, errors.New("invalid token")
+	}
+
+	if !parsedToken.Valid {
+		return nil, errors.New("token is invalid")
+	}
+
+	return parsedToken, nil
 }
