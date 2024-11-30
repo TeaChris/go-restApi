4b8d8d44af9a4ac8227e2df0d1b7d976e0202504
bermuda <olasunkanmiboluwatife110@gmail.com>
2024-11-30 07:58:26 +0100

    events

--- models/events.go
+++ models/events.go
@@ -11,12 +11,12 @@ type Event struct {
 	Description string 		`binding:"required"`
 	Location 	string		`binding:"required"`
 	DateTime 	time.Time	`binding:"required"`
-	UserId 		int			
+	UserId 		int64			
 }
 
 var events = []Event{}
 
-func (e Event) Save() error {
+func (e *Event) Save() error {
 	query := `
 	INSERT INTO events (name, description, location, date_time, user_id) 
 	VALUES (?, ?, ?, ?, ?)`
