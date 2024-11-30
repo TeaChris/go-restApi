f4d994c3a4962709c8f62895ea4ee83d530f7d23
bermuda <olasunkanmiboluwatife110@gmail.com>
2024-11-30 07:58:51 +0100

    generate token

--- utils/jwt.go
+++ utils/jwt.go
@@ -21,25 +21,39 @@ func GenerateToken(email string, userId int64) (string, error) {
 }
 
 
-func VerifyToken(token string) (*jwt.Token, error) {
+func VerifyToken(token string) (int64, error) {
 	parsedToken, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
-		_, ok := token.Method.(*jwt.SigningMethodHMAC)
-
+		_, ok := token.Method.(*jwt.SigningMethodHMAC); 
+		
 		if !ok {
 			return nil, errors.New("unexpected signing method")
 		}
 
 		return []byte(secret), nil
 	})
-
+		
 	if err != nil {
 		fmt.Println(err)
-		return nil, errors.New("invalid token")
+		return 0, errors.New("invalid token")
 	}
 
-	if !parsedToken.Valid {
-		return nil, errors.New("token is invalid")
+	tokenIsValid := parsedToken.Valid
+
+	if !tokenIsValid {
+		return 0, errors.New("token is invalid")
 	}
 
-	return parsedToken, nil
+
+	claims, ok := parsedToken.Claims.(jwt.MapClaims)
+
+	if !ok {
+		return 0, errors.New("token claims are not valid")
+	}
+
+	// email := claims["email"].(string)
+	userId := int64(claims["userId"].(float64))
+
+	return userId, nil
+
 }
+
